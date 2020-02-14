5bdaee67fecc1e196776271cbdbb4bd43efa86de
nanofuxion <nanofuxion@users.noreply.github.com>
2020-02-14 14:30:19 -0500

    first commit

---
+++ api/src/controllers/userController.ts
@@ -0,0 +1,5 @@
+import * as db from "../models/users";
+// MVC model - V
+
+
+
--- api/src/models/index.ts
+++ api/src/models/index.ts
@@ -1,4 +1,6 @@
 import { UserModel, UserSchema } from './users';
+import * as MongoClient from "mongodb";
+import * as mongoose from "mongoose";
 
 export interface IModels {
     user: UserModel;
@@ -6,6 +8,21 @@ export interface IModels {
 export const Models = async (dbUrl: string): Promise<IModels> => {
     try {
         // TODO: Connect to chosen database, and pass model objects
+        //connect
+
+        //verifying env value is passing to code  
+        console.log(`the db url is ${dbUrl}`);
+
+        //mongodb future proofing for module refactor 
+        mongoose.set('useNewUrlParser', true);
+        mongoose.set('useUnifiedTopology', true);
+
+        //connect to docker db
+        mongoose.connect(dbUrl, (e: any) => {
+            if(e) console.log(e.message);
+            else console.log("If this loaded, successfully mongoDB from docker, Noice.")
+        });
+
         const models = {
             user: new UserModel(),
         };
